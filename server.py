8f98b10f1a4752e0ec07ba32ab4cda19d1f9bf8b
Craig Allwardt <craig.allwardt@pnnl.gov>
2021-01-22 14:58:20 -0800

    Updated to use the IEEE2030_5 endpoints module and show in the /docs endpoint

--- server.py
+++ server.py
@@ -3,9 +3,20 @@ from typing import Optional
 from fastapi import FastAPI
 
 from IEEE2030_5 import IEEE2030_5_ENDPOINTS
+import uvicorn
 
 app = FastAPI()
 
+
+def mycallback():
+    return {"woot": "there it is!"}
+
+
+def load_endpoints(app):
+    for d, endpoint in IEEE2030_5_ENDPOINTS.items():
+        # TODO modify endpoint.callback to be an actual function
+        # This is probably not done here but in the IEEE2030_5_ENDPOINTS module.
+        app.add_api_route(endpoint.url, mycallback)
 
 
 @app.get("/")
@@ -17,3 +28,8 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+
+if __name__ == "__main__":
+    load_endpoints(app)
+    app.openapi()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
