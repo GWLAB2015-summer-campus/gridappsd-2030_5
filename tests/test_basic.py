d8293aeffadefaf699a165dd3deb566df2fe062f
C. Allwardt <3979063+craig8@users.noreply.github.com>
2021-12-07 11:10:15 -0800

    Added tests to the repository to go with SunSpec Conformance Test Procedures

---
+++ tests/test_basic.py
@@ -0,0 +1,88 @@
+def test_basic_001():
+    raise NotImplementedError()
+
+def test_basic_002():
+    raise NotImplementedError()
+
+def test_basic_003():
+    raise NotImplementedError()
+
+def test_basic_004():
+    raise NotImplementedError()
+
+def test_basic_005():
+    raise NotImplementedError()
+
+def test_basic_006():
+    raise NotImplementedError()
+
+def test_basic_007():
+    raise NotImplementedError()
+
+def test_basic_008():
+    raise NotImplementedError()
+
+def test_basic_009():
+    raise NotImplementedError()
+
+def test_basic_010():
+    raise NotImplementedError()
+
+def test_basic_011():
+    raise NotImplementedError()
+
+def test_basic_012():
+    raise NotImplementedError()
+
+def test_basic_013():
+    raise NotImplementedError()
+
+def test_basic_014():
+    raise NotImplementedError()
+
+def test_basic_015():
+    raise NotImplementedError()
+
+def test_basic_016():
+    raise NotImplementedError()
+
+def test_basic_017():
+    raise NotImplementedError()
+
+def test_basic_018():
+    raise NotImplementedError()
+
+def test_basic_019():
+    raise NotImplementedError()
+
+def test_basic_020():
+    raise NotImplementedError()
+
+def test_basic_021():
+    raise NotImplementedError()
+
+def test_basic_022():
+    raise NotImplementedError()
+
+def test_basic_023():
+    raise NotImplementedError()
+
+def test_basic_024():
+    raise NotImplementedError()
+
+def test_basic_025():
+    raise NotImplementedError()
+
+def test_basic_026():
+    raise NotImplementedError()
+
+def test_basic_027():
+    raise NotImplementedError()
+
+def test_basic_028():
+    raise NotImplementedError()
+
+def test_basic_029():
+    raise NotImplementedError()
+
+
---
+++ tests/test_core.py
@@ -0,0 +1,48 @@
+def test_core_001():
+    raise NotImplementedError()
+
+def test_core_002():
+    raise NotImplementedError()
+
+def test_core_003():
+    raise NotImplementedError()
+
+def test_core_004():
+    raise NotImplementedError()
+
+def test_core_005():
+    raise NotImplementedError()
+
+def test_core_006():
+    raise NotImplementedError()
+
+def test_core_009():
+    raise NotImplementedError()
+
+def test_core_010():
+    raise NotImplementedError()
+
+def test_core_011():
+    raise NotImplementedError()
+
+def test_core_012():
+    raise NotImplementedError()
+
+def test_core_013():
+    raise NotImplementedError()
+
+def test_core_014():
+    raise NotImplementedError()
+
+def test_core_018():
+    raise NotImplementedError()
+
+def test_core_019():
+    raise NotImplementedError()
+
+def test_core_021():
+    raise NotImplementedError()
+
+def test_core_022():
+    raise NotImplementedError()
+
