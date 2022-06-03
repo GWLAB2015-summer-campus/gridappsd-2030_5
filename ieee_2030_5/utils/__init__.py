a76b4593e583173885ee9df0105a7d42ef483f69
C. Allwardt <3979063+craig8@users.noreply.github.com>
2022-06-03 10:21:10 -0700

    dataclass_to_xml returns string rather than Response

--- ieee_2030_5/utils/__init__.py
+++ ieee_2030_5/utils/__init__.py
@@ -5,5 +5,5 @@ from flask import Response
 from ieee_2030_5.models.serializer import serialize_xml
 
 
-def dataclass_to_xml(dc: dataclass) -> Response:
-    return Response(serialize_xml(dc), mimetype="text/xml")
+def dataclass_to_xml(dc: dataclass) -> str:
+    return serialize_xml(dc)
