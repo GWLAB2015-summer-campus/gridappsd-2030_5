528d6caabd309133878ae4ba6c9eca864beec4ff
C. Allwardt <3979063+craig8@users.noreply.github.com>
2022-07-20 11:02:35 -0700

    Fix parser and serializer schema handling Only have the "urn:ieee:std:2030.5:ns" as default namespace

--- ieee_2030_5/utils/__init__.py
+++ ieee_2030_5/utils/__init__.py
@@ -1,16 +1,19 @@
 from dataclasses import dataclass
+from typing import Type, Optional
 
 from xsdata.formats.dataclass.context import XmlContext
+from xsdata.formats.dataclass.parsers.config import ParserConfig
 from xsdata.formats.dataclass.parsers.xml import XmlParser
 from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
 
 __xml_context__ = XmlContext()
-__xml_parser__ = XmlParser(context=__xml_context__)
-
-__config__ = SerializerConfig(xml_declaration=True, )
+__parser_config__ = ParserConfig(fail_on_unknown_attributes=False,
+                                 fail_on_unknown_properties=False)
+__xml_parser__ = XmlParser(config=__parser_config__, context=__xml_context__)
+__config__ = SerializerConfig(xml_declaration=True, pretty_print=True)
 __serializer__ = XmlSerializer(config=__config__)
-__ns_map__ = {"xmlns": "http://zigbee.org/sep", "xsi": "http://www.w3.org/2001/XMLSchema-instance"}
+__ns_map__ = {None: "urn:ieee:std:2030.5:ns"}
 
 
 def serialize_dataclass(obj: dataclass) -> str:
@@ -18,14 +21,14 @@ def serialize_dataclass(obj: dataclass) -> str:
     Serializes a dataclass that was created via xsdata to an xml string for
     returning to a client.
     """
-    return __serializer__.render(obj)
+    return __serializer__.render(obj, ns_map=__ns_map__)
 
 
-def parse_xml(xml: str) -> dataclass:
+def parse_xml(xml: str, type: Optional[Type] = None) -> dataclass:
     """
     Parse the xml passed and return result from loaded classes.
     """
-    return __xml_parser__.from_string(xml)
+    return __xml_parser__.from_string(xml, type)
 
 
 def dataclass_to_xml(dc: dataclass) -> str:
