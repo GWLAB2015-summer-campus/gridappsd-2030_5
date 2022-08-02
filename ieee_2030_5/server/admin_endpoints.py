d38cf3fdda88db7fdca7b7edcfb9eb392ea8b249
C. Allwardt <3979063+craig8@users.noreply.github.com>
2022-08-02 14:22:04 -0700

    Add admin pages for program lists and end devices.

--- ieee_2030_5/server/admin_endpoints.py
+++ ieee_2030_5/server/admin_endpoints.py
@@ -1,10 +1,10 @@
 import json
 
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 from ieee_2030_5.certs import TLSRepository
 from ieee_2030_5.config import ServerConfiguration
-from ieee_2030_5.models.end_devices import EndDevices
+from ieee_2030_5.server.server_constructs import EndDevices
 
 
 class AdminEndpoints:
@@ -13,7 +13,10 @@ class AdminEndpoints:
         self.tls_repo = tls_repo
         self.server_config = config
 
+        app.add_url_rule("/admin/end-device-list", view_func=self._end_device_list)
+        app.add_url_rule("/admin/program-lists", view_func=self._program_lists)
         app.add_url_rule("/admin/lfid", endpoint="admin/lfid", view_func=self._lfid_lists)
+        app.add_url_rule("/admin/edev/<int:edevid>/fsa", view_func=self._edev_fsa)
 
     def _lfid_lists(self) -> Response:
         items = []
@@ -22,3 +25,15 @@ class AdminEndpoints:
             items.append({"key": k, "lfid": int(v.end_device.lFDI)})
 
         return Response(json.dumps(items))
+
+    def _edev_fsa(self, edevid: int) -> Response:
+        #edev = self.end_devices.get(edevid)
+        return Response(json.dumps(json.dumps(self.end_devices.get_fsa_list(edevid=edevid))))
+
+    def _program_lists(self) -> str:
+        return render_template("admin/program-lists.html",
+                               program_lists=self.server_config.program_lists)
+
+    def _end_device_list(self) -> str:
+        return render_template("admin/end-device-list.html",
+                               end_device_list=self.end_devices.get_end_devices())
