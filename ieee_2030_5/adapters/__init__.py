4149e7b740c6099a3337f500a7e0956396c68b09
C <3979063+craig8@users.noreply.github.com>
2023-10-12 22:42:46 -0700

    update adapters

--- ieee_2030_5/adapters/__init__.py
+++ ieee_2030_5/adapters/__init__.py
@@ -7,8 +7,8 @@ from copy import deepcopy
 from dataclasses import dataclass, fields, is_dataclass
 from enum import Enum
 from pathlib import Path
-from typing import (Any, ClassVar, Dict, Generic, List, Optional, Protocol,
-                    Type, TypeVar, Union, get_args, get_origin)
+from typing import (Any, ClassVar, Dict, Generic, List, Optional, Protocol, Type, TypeVar, Union,
+                    get_args, get_origin)
 
 import yaml
 from blinker import Signal
@@ -20,52 +20,76 @@ from ieee_2030_5.certs import TLSRepository
 
 _log = logging.getLogger(__name__)
 
+
 class AlreadyExists(Exception):
     pass
 
+
+class NotFoundError(Exception):
+    pass
+
+
 load_event = Signal("load-store-event")
 store_event = Signal("store-data-event")
+
 
 def __get_store__(store_name: str) -> Path:
     if cfg.ServerConfiguration.storage_path is None:
         cfg.ServerConfiguration.storage_path = Path("data_store")
     elif isinstance(cfg.ServerConfiguration.storage_path, str):
         cfg.ServerConfiguration.storage_path = Path(cfg.ServerConfiguration.storage_path)
-    
+
     store_path = cfg.ServerConfiguration.storage_path
     store_path.mkdir(parents=True, exist_ok=True)
     store_path = store_path / f"{store_name}.yml"
-    
+
     return store_path
 
-def do_load_event(caller: Adapter) -> None:
-    """Load an adaptor type from the data_store path.
-    
-    
-    """
-    _log.debug(f"Loading store {caller.generic_type_name}")
-    store = __get_store__(caller.generic_type_name)
-    
-    if not store.exists():
-        _log.debug(f"Store {store.as_posix()} does not exist at present.")
-        return
-    
-    # Load from yaml unsafe values etc.
-    with open(store, "r") as f:
-        items = yaml.load(f, Loader=yaml.UnsafeLoader)
-        
-        caller.__dict__.update(items)
-    _log.debug(f"Loaded {len(caller._item_list)} items from store")
-    
-def do_save_event(caller: Adapter) -> None:
-    _log.debug(f"Storing: {caller.generic_type_name}")
-    store = __get_store__(caller.generic_type_name)
-    
-    with open(store, 'w') as f:
-        yaml.dump(caller.__dict__, f, default_flow_style=False, allow_unicode=True)
-    
 
-load_event.connect(do_load_event)    
+def do_load_event(caller: Union[Adapter, GenericListAdapter]) -> None:
+    """Load an adaptor type from the data_store path.
+
+
+    """
+    store_file = None
+    if isinstance(caller, Adapter):
+        _log.debug(f"Loading store {caller.generic_type_name}")
+        store_file = __get_store__(caller.generic_type_name)
+    elif isinstance(caller, GenericListAdapter):
+        store_file = __get_store__(caller.__class__.__name__)
+    else:
+        raise ValueError(f"Invalid caller type {type(caller)}")
+
+    if not store_file.exists():
+        _log.debug(f"Store {store_file.as_posix()} does not exist at present.")
+        return
+
+    # Load from yaml unsafe values etc.
+    with open(store_file, "r") as f:
+        items = yaml.load(f, Loader=yaml.UnsafeLoader)
+
+        caller.__dict__.update(items)
+
+    _log.debug(f"Loaded {caller.count} items from store")
+
+
+def do_save_event(caller: Union[Adapter, GenericListAdapter]) -> None:
+
+    store_file = None
+    if isinstance(caller, Adapter):
+        _log.debug(f"Loading store {caller.generic_type_name}")
+        store_file = __get_store__(caller.generic_type_name)
+        _log.debug(f"Storing: {caller.generic_type_name}")
+    elif isinstance(caller, GenericListAdapter):
+        store_file = __get_store__(caller.__class__.__name__)
+    else:
+        raise ValueError(f"Invalid caller type {type(caller)}")
+
+    with open(store_file, 'w') as f:
+        yaml.dump(caller.__dict__, f, default_flow_style=False, allow_unicode=True)
+
+
+load_event.connect(do_load_event)
 store_event.connect(do_save_event)
 
 
@@ -96,19 +120,21 @@ def populate_from_kwargs(obj: object, **kwargs) -> Dict[str, Any]:
             kwargs.pop(k.name)
     return kwargs
 
+
 class AdapterIndexProtocol(Protocol):
-    
+
     def fetch_at(self, index: int) -> m.Resource:
         pass
 
 
 class AdapterListProtocol(AdapterIndexProtocol):
-    def fetch_list(self, start: int = 0, after: int =0, limit:int = 0) -> m.List_type:
+
+    def fetch_list(self, start: int = 0, after: int = 0, limit: int = 0) -> m.List_type:
         pass
-    
+
     def fetch_edev_all(self) -> List:
         pass
-        
+
 
 ready_signal = Signal("ready-signal")
 
@@ -116,10 +142,12 @@ T = TypeVar('T')
 C = TypeVar('C')
 D = TypeVar('D')
 
-class ListAdpater:
-    def __init__(self):        
+
+class GenericListAdapter:
+
+    def __init__(self):
         self._list_urls = []
-        self._list_containers: Dict[str, List] = {}
+        self._list_containers: Dict[str, List[D]] = {}
         self._types: Dict[str, D] = {}
         load_event.send(self)
         # if "generic_type" not in kwargs:
@@ -130,86 +158,117 @@ class ListAdpater:
         #     self._container = container()
         # else:
         #     self._container = container
-        
+
         # setattr(self._container, T.__name__, [])
-    @property
-    def generic_type_name(self):
-        return self.__class__.__name__
-    
+
+    def count(self) -> int:
+        count_of = 0
+        for v in self._list_containers.values():
+            count_of += len(v)
+        return count_of
+
+    def get_type(self, uri: str) -> D:
+        return type(self._types.get(uri))
+
     def initialize_uri(self, uri: str, obj: D):
         if self._list_containers.get(uri) and self._types.get(uri) != obj:
             _log.error("Must initialize before container has any items.")
             raise ValueError("Must initialize before container has any items.")
         self._types[uri] = obj
-        
-    
+
     def append(self, uri: str, obj: D):
         # if there is a type
         expected_type = self._types.get(uri)
         if expected_type:
-            assert isinstance(obj, expected_type)
-        
+            assert isinstance(obj, type(expected_type))
+        else:
+            self.initialize_uri(uri, obj)
+
         if uri not in self._list_containers:
             self._list_containers[uri] = []
         self._list_containers[uri].append(obj)
         store_event.send(self)
-        
+
+    def get_item_by_prop(self, uri: str, prop: str, value: Any) -> D:
+        _list = self._list_containers[uri]
+        for item in _list:
+            if getattr(item, prop) == value:
+                return item
+        raise NotFoundError(f"Uri {uri} does not contain {prop} == {value}")
+
+    def has_list(self, uri: str) -> bool:
+        return uri in self._list_containers
+
+    def get_list(self, uri: str, order_prop: Optional[str] = None):
+        return self._list_containers[uri]
+
     def remove(self, uri: str, index: int):
         del self._list_containers[uri][index]
         store_event.send(self)
-        
+
     def render_container(self, uri: str, instance: object, prop: str):
         setattr(instance, prop, deepcopy(self._list_containers[uri]))
-        
+
     def print_container(self, uri: str):
         pprint(self._list_containers[uri])
-        
+
+    def clear_all(self):
+        self._list_containers.clear()
+
+    def clear(self, uri: str):
+        if uri in self._list_containers:
+            self._list_containers[uri].clear()
 
 
 class Adapter(Generic[T]):
-    
+
     def __init__(self, url_prefix: str, **kwargs):
         if "generic_type" not in kwargs:
             raise ValueError("Missing generic_type parameter")
         self._generic_type: Type = kwargs['generic_type']
         self._href_prefix: str = url_prefix
-        self._current_index: int  = -1
+        self._current_index: int = -1
         self._item_list: Dict[int, T] = {}
         _log.debug(f"Intializing adapter {self.generic_type_name}")
         load_event.send(self)
-    
+
+    @property
+    def count(self) -> int:
+        return len(self._item_list)
+
     @property
     def generic_type_name(self) -> str:
         return self._generic_type.__name__
+
     @property
     def href_prefix(self) -> str:
         return self._href_prefix
-    
+
     @property
     def href(self) -> str:
         return self._href_prefix
-    
+
     @href.setter
-    def href(self, value:str) -> None:
+    def href(self, value: str) -> None:
         self._href_prefix = value
-        
+
     def clear(self) -> None:
         self._current_index = -1
         self._item_list: Dict[int, T] = {}
         store_event.send(self)
-        
+
     def fetch_by_mrid(self, mrid: str) -> Optional[T]:
         return self.fetch_by_property("mRID", mrid)
-        
+
     def fetch_by_href(self, href: str) -> Optional[T]:
         return self.fetch_by_property("href", href)
-    
+
     def fetch_by_property(self, prop: str, prop_value: Any) -> Optional[T]:
         for obj in self._item_list.values():
             # Most properties are pointers to other objects so we are going to
             # check both the property and the sub object property here, because
-            # that should save some of the time later when we are looking for 
-            # hrefs and and can't get to them because they are wrapped in a 
+            # that should save some of the time later when we are looking for
+            # hrefs and and can't get to them because they are wrapped in a
             # Link object.
             under_test = getattr(obj, prop)
             if isinstance(under_test, str):
@@ -218,33 +277,38 @@ class Adapter(Generic[T]):
             else:
                 if getattr(under_test, prop) == prop_value:
                     return obj
-            
+
     def add(self, item: T) -> T:
         if not isinstance(item, self._generic_type):
             raise ValueError(f"Item {item} is not of type {self._generic_type}")
-        
+
         # Only replace if href is specified.
         if hasattr(item, 'href') and getattr(item, 'href') is None:
-            setattr(item, 'href', hrefs.SEP.join([self._href_prefix, str(self._current_index + 1)]))
+            setattr(item, 'href', hrefs.SEP.join([self._href_prefix,
+                                                  str(self._current_index + 1)]))
         self._current_index += 1
         self._item_list[self._current_index] = item
-        
+
         store_event.send(self)
         return item
-    
-    def fetch_all(self, container: Optional[D] = None, start: int = 0, after: int = 0, limit: int = 1) -> D:
+
+    def fetch_all(self,
+                  container: Optional[D] = None,
+                  start: int = 0,
+                  after: int = 0,
+                  limit: int = 1) -> D:
 
         if container is not None:
             if not container.__class__.__name__.endswith("List"):
                 raise ValueError("Must have List as the last portion of the name for instance")
-        
+
             prop_found = container.__class__.__name__[:container.__class__.__name__.find("List")]
-        
+
             items = list(self._item_list.values())
             all_len = len(items)
             all_results = len(items)
             all_items = items
-                
+
             if start > len(items):
                 all_items = []
                 all_results = 0
@@ -252,21 +316,21 @@ class Adapter(Generic[T]):
                 if limit == 0:
                     all_items = items[start:]
                 else:
-                    all_items = items[start: start + limit]
+                    all_items = items[start:start + limit]
                 all_results = len(all_items)
-                
+
             setattr(container, prop_found, all_items)
             setattr(container, "all", all_len)
             setattr(container, "results", all_results)
         else:
             container = list(self._item_list.values())
-            
+
         return container
-    
+
     def fetch_index(self, obj: T, using_prop: str = None) -> int:
         found_index = -1
         for index, obj1 in self._item_list.items():
-            if using_prop is None:    
+            if using_prop is None:
                 if obj1 == obj:
                     found_index = index
                     break
@@ -277,58 +341,50 @@ class Adapter(Generic[T]):
         if found_index == -1:
             raise KeyError(f"Object {obj} not found in adapter")
         return found_index
-    
+
     def fetch(self, index: int):
         return self._item_list[index]
-    
+
     def put(self, index: int, obj: T):
         self._item_list[index] = obj
         store_event.send(self)
-    
+
     def fetch_by_mrid(self, mRID: str):
         for item in self._item_list.values():
             if not hasattr(item, 'mRID'):
                 raise ValueError(f"Item of {type(T)} does not have mRID property")
             if item.mRID == mRID:
                 return item
-            
-        raise KeyError()
-    
+
+        raise KeyError(f"mRID ({mRID}) not found.")
+
     def size(self) -> int:
         return len(self._item_list)
-    
-        
-from ieee_2030_5.adapters.adapters import (DERAdapter, 
-                                           DERControlAdapter,
-                                           DERCurveAdapter, DERProgramAdapter,
-                                           DeviceCapabilityAdapter,
-                                           EndDeviceAdapter,
-                                           FunctionSetAssignmentsAdapter,
-                                           RegistrationAdapter,
-                                           MirrorUsagePointAdapter,
-                                           UsagePointAdapter,
-                                           create_mirror_usage_point,
-                                           create_mirror_meter_reading)
+
+
+from ieee_2030_5.adapters.adapters import (DERAdapter, DERControlAdapter, DERCurveAdapter,
+                                           DERProgramAdapter, DeviceCapabilityAdapter,
+                                           EndDeviceAdapter, FunctionSetAssignmentsAdapter,
+                                           RegistrationAdapter, MirrorUsagePointAdapter,
+                                           UsagePointAdapter, TimeAdapter, ListAdapter,
+                                           create_mirror_usage_point, create_mirror_meter_reading)
 
 __all__ = [
-    'DERControlAdapter',
-    'DERCurveAdapter',
-    'DERProgramAdapter',
-    'DeviceCapabilityAdapter',
-    'EndDeviceAdapter',
-    'FunctionSetAssignmentsAdapter',
-    'RegistrationAdapter',
-    'DERAdapter',
-    'MirrorUsagePointAdapter',
-    'UsagePointAdapter',
-    'create_mirror_usage_point',
-    'create_mirror_meter_reading'
+    'DERControlAdapter', 'DERCurveAdapter', 'DERProgramAdapter', 'DeviceCapabilityAdapter',
+    'EndDeviceAdapter', 'FunctionSetAssignmentsAdapter', 'RegistrationAdapter', 'DERAdapter',
+    'MirrorUsagePointAdapter', 'TimeAdapter', 'UsagePointAdapter', 'create_mirror_usage_point',
+    'create_mirror_meter_reading', 'ListAdapter'
 ]
+
 
 def clear_all_adapters():
     for adpt in __all__:
         obj = eval(adpt)
         if isinstance(obj, Adapter):
             obj.clear()
+        elif isinstance(obj, GenericListAdapter):
+            obj.clear_all()
+
+
 # from ieee_2030_5.adapters.log import LogAdapter
 # from ieee_2030_5.adapters.mupupt import MirrorUsagePointAdapter
--- ieee_2030_5/adapters/adapters.py
+++ ieee_2030_5/adapters/adapters.py
@@ -1,34 +1,47 @@
-from typing import Union
+from typing import Dict, Union
 from ieee_2030_5.data.indexer import add_href
 import ieee_2030_5.hrefs as hrefs
 import ieee_2030_5.models as m
-from ieee_2030_5.adapters import Adapter
+from ieee_2030_5.adapters import Adapter, GenericListAdapter
 from ieee_2030_5.config import ReturnValue
+
+import gevent
+from datetime import datetime
+from threading import Thread
+import time
+
+from blinker import Signal
 import logging
+
 _log = logging.getLogger(__name__)
 
 DERCurveAdapter = Adapter[m.DERCurve](hrefs.curve_href(), generic_type=m.DERCurve)
-
 DERControlAdapter = Adapter[m.DERControl]("/derc", generic_type=m.DERControl)
 DERCurveAdapter = Adapter[m.DERCurve](hrefs.curve_href(), generic_type=m.DERCurve)
 DERProgramAdapter = Adapter[m.DERProgram](hrefs.der_program_href(), generic_type=m.DERProgram)
-FunctionSetAssignmentsAdapter = Adapter[m.FunctionSetAssignments](url_prefix="/fsa", generic_type=m.FunctionSetAssignments)
+FunctionSetAssignmentsAdapter = Adapter[m.FunctionSetAssignments](
+    url_prefix="/fsa", generic_type=m.FunctionSetAssignments)
 
 EndDeviceAdapter = Adapter[m.EndDevice](hrefs.get_enddevice_href(), generic_type=m.EndDevice)
 DeviceCapabilityAdapter = Adapter[m.DeviceCapability]("/dcap", generic_type=m.DeviceCapability)
 # Generally the href will only be in the context of an end device.
 RegistrationAdapter = Adapter[m.Registration](url_prefix="/reg", generic_type=m.Registration)
 DERAdapter = Adapter[m.DER](url_prefix="/der", generic_type=m.DER)
-MirrorUsagePointAdapter = Adapter[m.MirrorUsagePoint](url_prefix="/mup", generic_type=m.MirrorUsagePoint)
-MirrorMeterReadingAdapter = Adapter[m.MirrorMeterReading](url_prefix="/mr", generic_type=m.MirrorMeterReading)
-MirrorReadingSetAdapter = Adapter[m.MirrorReadingSet](url_prefix="/rs", generic_type=m.MirrorReadingSet)
+MirrorUsagePointAdapter = Adapter[m.MirrorUsagePoint](url_prefix="/mup",
+                                                      generic_type=m.MirrorUsagePoint)
+MirrorMeterReadingAdapter = Adapter[m.MirrorMeterReading](url_prefix="/mr",
+                                                          generic_type=m.MirrorMeterReading)
+MirrorReadingSetAdapter = Adapter[m.MirrorReadingSet](url_prefix="/rs",
+                                                      generic_type=m.MirrorReadingSet)
 UsagePointAdapter = Adapter[m.UsagePoint](url_prefix="/upt", generic_type=m.UsagePoint)
+ListAdapter = GenericListAdapter()
 
 
-def create_mirror_meter_reading(mup_href: str, mr: Union[m.MirrorMeterReading, m.MirrorMeterReadingList]) -> ReturnValue:
+def create_mirror_meter_reading(
+        mup_href: str, mr: Union[m.MirrorMeterReading, m.MirrorMeterReadingList]) -> ReturnValue:
     upt_href = hrefs.UsagePointHref()
     upt_index = int(mup_href.split(hrefs.SEP)[-1])
-    
+
     was_updated = False
     was_success = True
     try:
@@ -39,55 +52,52 @@ def create_mirror_meter_reading(mup_href: str, mr: Union[m.MirrorMeterReading, m
         mr.href = upt_href.meterreading(upt_index, MirrorMeterReadingAdapter.size())
         found_mirror_reading = MirrorMeterReadingAdapter.add(mr)
         mr_index = int(mr.href.split(hrefs.SEP)[-1])
-    
+
     if isinstance(mr, m.MirrorMeterReadingList):
         raise NotImplemented()
-            
-        
-    elif isinstance(mr, m.MirrorMeterReading):        
+
+    elif isinstance(mr, m.MirrorMeterReading):
         for rs_index, rs in enumerate(found_mirror_reading.MirrorReadingSet):
             try:
                 found_rs = MirrorReadingSetAdapter.fetch_by_mrid(rs.mRID)
                 was_updated = True
             except KeyError:
                 found_rs = None
-            
+
             if not found_rs:
                 rs.href = upt_href.readingset(upt_index, mr_index, rs_index)
                 found_rs = MirrorReadingSetAdapter.add(rs)
-            else:               
+            else:
                 found_rs.description = rs.description
                 found_rs.timePeriod = rs.timePeriod
                 found_rs.version = rs.version
-                
+
             for r_index, r in enumerate(rs.Reading):
                 r.href = upt_href.readingsetreading(upt_index, mr_index, rs_index, r_index)
-                
+
     return ReturnValue(was_success, found_mirror_reading, was_updated)
-    
 
 
 def create_mirror_usage_point(mup: m.MirrorUsagePoint) -> ReturnValue:
     """Creates a MirrorUsagePoint and associated UsagePoint and adds them to their adapters."""
-    
+
     # assert mup.href is None, "Cannot create a new mirror usage point with an href set already."
     found_with_mrid = None
-    if MirrorUsagePointAdapter.size() > 0:    
+    if MirrorUsagePointAdapter.size() > 0:
         try:
             found_with_mrid = MirrorUsagePointAdapter.fetch_by_mrid(mup.mRID)
         except KeyError:
             ...
-    
-    
+
     if not found_with_mrid:
         # After this call the href is populated with the mup
         mup = MirrorUsagePointAdapter.add(mup)
         usage_point_index = int(mup.href.split(hrefs.SEP)[-1])
-        
+
         upt_href = hrefs.UsagePointHref()
-        
+
         meter_reading_list = m.MeterReadingList(href=upt_href.meterreading_list(usage_point_index))
-                        
+
         for index, mirror_reading in enumerate(mup.MirrorMeterReading):
             if mirror_reading.ReadingType is None:
                 return ReturnValue(False, "Invalid Reading Type")
@@ -96,26 +106,27 @@ def create_mirror_usage_point(mup: m.MirrorUsagePoint) -> ReturnValue:
             reading = m.MeterReading(upt_href.meterreading(usage_point_index, index))
             reading.mRID = mirror_reading.mRID
             reading.description = mirror_reading.description
-            
-            
+
             if mirror_reading.Reading is not None:
                 # These links are going to be handled by the add_href / get_href functions.
                 reading.ReadingLink = m.ReadingLink(upt_href.reading(index))
                 add_href(upt_href.reading(index), mirror_reading.Reading)
-                
-            reading.ReadingTypeLink = m.ReadingTypeLink(upt_href.readingtype(usage_point_index, index))
+
+            reading.ReadingTypeLink = m.ReadingTypeLink(
+                upt_href.readingtype(usage_point_index, index))
             add_href(upt_href.readingtype(usage_point_index, index), mirror_reading.ReadingType)
-                        
+
             meter_reading_list.MeterReading.append(reading)
-        
-        upt = UsagePointAdapter.add(m.UsagePoint(href=upt_href,
-                                           description=mup.description,
-                                           deviceLFDI=mup.deviceLFDI,
-                                           serviceCategoryKind=mup.serviceCategoryKind,
-                                           mRID=mup.mRID,
-                                           roleFlags=mup.roleFlags,
-                                           status=mup.status,
-                                           MeterReadingListLink=m.MeterReadingListLink(meter_reading_list.href)))
+
+        upt = UsagePointAdapter.add(
+            m.UsagePoint(href=upt_href,
+                         description=mup.description,
+                         deviceLFDI=mup.deviceLFDI,
+                         serviceCategoryKind=mup.serviceCategoryKind,
+                         mRID=mup.mRID,
+                         roleFlags=mup.roleFlags,
+                         status=mup.status,
+                         MeterReadingListLink=m.MeterReadingListLink(meter_reading_list.href)))
         _log.debug(f"Storing meter_reading_list {meter_reading_list.href}")
         add_href(meter_reading_list.href, meter_reading_list)
         update = False
@@ -129,33 +140,68 @@ def create_mirror_usage_point(mup: m.MirrorUsagePoint) -> ReturnValue:
         found_with_mrid.roleFlags = mup.roleFlags
         found_with_mrid.status = mup.status
         update = True
-    
+
     return ReturnValue(True, mup, update)
-    
-        
-    
-    
-    
-    # def create(self, mup: m.MirrorUsagePoint) -> Tuple[ReturnCode, str]:
-    #     """Creates a MirrorUsagePoint and its associated usage point
-        
-    #     This method creates all of the sub elements of the usage point and
-    #     mirror usage points as well.
-    #     """
-        
-    #     before = len(self.__upt_container__)
-    #     upt = self.__upt_container__.create_or_replace(mup)
-    #     after = len(self.__upt_container__)
-    #     if after > before:
-    #         # TODO: Don't hard code here.
-    #         mup.href = upt.href.replace('upt', 'mup')
-    #         mup.postRate = BaseAdapter.server_config().usage_point_post_rate
-    #         self.__mirror_usage_points__.append(mup)
-    #         return ReturnCode.CREATED.value, mup.href
-    #     else:
-    #         for i, o in enumerate(self.__mirror_usage_points__):
-    #             if o.mRID == mup.mRID:
-    #                 mup.href = o.href
-    #                 self.__mirror_usage_points__[i] = mup
-    #                 break
-    #         return ReturnCode.NO_CONTENT.value, mup.href
+
+
+class _TimeAdapter(Thread):
+    tick = Signal("tick")
+    event_started = Signal("event_started")
+    event_ended = Signal("event_endend")
+    event_scheduled = Signal("event_scheduled")
+    events: Dict[str, m.Event] = {}
+    current_tick: int = 0
+    looping: bool = False
+
+    @staticmethod
+    def user_readable(timestamp: int) -> str:
+        dt = datetime.fromtimestamp(timestamp)
+        return dt.isoformat()    # .strftime("%m/%d/%Y, %H:%M:%S")
+
+    @staticmethod
+    def from_iso(iso_fmt_date: str) -> int:
+        dt = datetime.strptime(iso_fmt_date, "%Y-%m-%dT%H:%M:%S")
+        return int(time.mktime(dt.timetuple()))
+
+    @staticmethod
+    def add_event(evnt: m.Event):
+        time_now = _TimeAdapter.current_tick
+        if evnt.EventStatus is None:
+            evnt.EventStatus = m.EventStatus()
+        if evnt.href not in _TimeAdapter.events:
+            while _TimeAdapter.looping:
+                time.sleep(0.1)
+            _TimeAdapter.events[evnt.href] = evnt
+
+    def run(self) -> None:
+
+        while True:
+            _TimeAdapter.current_tick = int(time.mktime(datetime.utcnow().timetuple()))
+            _TimeAdapter.tick.send(self.current_tick)
+            _TimeAdapter.looping = True
+            time_now = _TimeAdapter.current_tick
+            for href, evnt in _TimeAdapter.events.items():
+                if time_now < evnt.interval.start and evnt.EventStatus.currentStatus is None:
+                    evnt.EventStatus.dateTime = time_now
+                    evnt.EventStatus.currentStatus = 0
+                    _log.debug(f"{'='*20}Event Scheduled {evnt.href}")
+                    _TimeAdapter.event_scheduled.send(evnt)
+                elif evnt.interval.start < time_now and time_now < evnt.interval.start + evnt.interval.duration:
+                    if evnt.EventStatus.currentStatus != 1:
+                        evnt.EventStatus.currentStatus = 1
+                        evnt.EventStatus.dateTime = time_now
+                        _log.debug(f"{'='*20}Event Started {evnt.href}")
+                        _TimeAdapter.event_started.send(evnt)
+                elif time_now > evnt.interval.start + evnt.interval.duration and evnt.EventStatus.currentStatus == 1:
+                    evnt.EventStatus.currentStatus = 5
+                    evnt.EventStatus.dateTime = time_now
+                    _log.debug(f"{'='*20}Event Complete {evnt.href}")
+                    _TimeAdapter.event_ended.send(evnt)
+
+            _TimeAdapter.looping = False
+            time.sleep(1)
+
+
+TimeAdapter = _TimeAdapter()
+TimeAdapter.daemon = True
+TimeAdapter.start()
