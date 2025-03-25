772dfc6a3cb7004eb6ab39cce87b0a6ddadcfb01
jasper200207 <jasper12394@gmail.com>
2025-03-25 04:09:35 +0000

    주석제거

--- ieee_2030_5/server/timefs.py
+++ ieee_2030_5/server/timefs.py
@@ -14,20 +14,12 @@ class TimeRequest(RequestOp):
         super().__init__(**kwargs)
 
     def get(self) -> Response:
-        # TODO fix for new stuff.
-        # local_tz = datetime.now().astimezone().tzinfo
-        # now_local = datetime.now().replace(tzinfo=local_tz)
-
-        #now_utc = datetime.utcnow().replace(tzinfo=pytz.utc)
         now_utc = datetime.utcnow().replace(tzinfo=zoneinfo.ZoneInfo('UTC'))
         
-        # now_utc = pytz.utc.localize(datetime.utcnow())
-        # local_tz = pytz.timezone(tzlocal.get_localzone().zone)
         local_tz = zoneinfo.ZoneInfo(tzlocal.get_localzone().zone)
 
         now_local = datetime.now().replace(tzinfo=local_tz)
 
-        # use zoneinfo get start_dst_utc and end_dst_utc
         start_dst_utc, end_dst_utc = [
             datetime.now().astimezone(local_tz).replace(tzinfo=zoneinfo.ZoneInfo('UTC'))
             for _ in range(2)
